2cea36e147f610874abe693b31e1a158cc6a894c
HenriP <paavilainenhenri@gmail.com>
2025-09-16 13:17:41 +0300

    complete palindrome-exercise

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,6 +1,17 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    const regex = /\s*[*!?\-,.\s*]\s*/gi;
 
+    const strReplace = str.replace(regex, "").toLowerCase();
+    const finalStr = strReplace.split("")
+    .reverse()
+    .join("");
+
+    console.log(finalStr);
+    console.log(strReplace);
+
+    return finalStr === strReplace;
 };
 
+console.log(palindromes("A car, a man, a maraca."));
 // Do not edit below this line
 module.exports = palindromes;
