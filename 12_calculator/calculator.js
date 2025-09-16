5fc163df5e3b58e088e34a9222370d44ac4cc0b9
HenriP <paavilainenhenri@gmail.com>
2025-09-16 12:24:07 +0300

    Complete calculator exercise

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,40 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+  return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  return arr.reduce((sum, currVal) => sum + currVal, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  if(arr == []){
+    return 0;
+  }
+  return arr.reduce((sum, currVal) => sum * currVal, 1);
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+  let power = 1;
+  let sum = 0;
+	for(i = 0; i < num2; i++){
+    power *= num1;
+    sum += power * num1
+  }
+  return power;
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+  let arr = [];
+  while(n > 0){
+    arr.unshift(n);
+    --n;
+  }
+  console.log(arr);
+return arr.reduce((prev, curr) => prev * curr, 1);
 };
 
 // Do not edit below this line
