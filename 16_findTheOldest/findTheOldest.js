3bb9c77bfd70e4030a0dd8b07a2a70f13491c65e
HenriP <paavilainenhenri@gmail.com>
2025-09-18 07:53:20 +0300

    Complete exercise findTheOldest

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,6 +1,29 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+    const date = new Date;
+    arr.forEach(item => {
+        if(!item.yearOfDeath){
+            item.yearOfDeath = date.getFullYear();
+        }
+    })
+    const oldest = arr.sort((a, b) => (a.yearOfBirth - a.yearOfDeath) - (b.yearOfBirth - b.yearOfDeath));
+    return oldest[0];
 };
-
+    const people = [
+      {
+        name: "Carly",
+        yearOfBirth: 2018,
+      },
+      {
+        name: "Ray",
+        yearOfBirth: 1962,
+        yearOfDeath: 2011,
+      },
+      {
+        name: "Jane",
+        yearOfBirth: 1912,
+        yearOfDeath: 1941,
+      },
+    ]
+console.log(people);
 // Do not edit below this line
 module.exports = findTheOldest;
